bf72ff6d7dc35f17cd6069359cc4df5ff839373a
kdog0625 <kdoooog34@gmail.com>
2020-06-14 01:46:18 +0900

    about

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,32 +1,32 @@
-$(document).on('turbolinks:load', function() {
-$(function() {
+// $(document).on('turbolinks:load', function() {
+// $(function() {
   
-  $('.nametitle').hover(
-    function(){
-      $('.about_name_shousai').fadeIn(3000);
-  },
-  function(){
-$('.about_name_shousai').fadeOut(2000);
-  }
-  );
+//   $('.nametitle').hover(
+//     function(){
+//       $('.about_name_shousai').fadeIn(3000);
+//   },
+//   function(){
+// $('.about_name_shousai').fadeOut(2000);
+//   }
+//   );
   
-  $('.nametitle').hover(
-    function(){
-      $('.about_name_shousai1').fadeIn(4000);
-  },
-  function(){
-$('.about_name_shousai1').fadeOut(1000);
-  }
-  );
-  $('.aboutbtn_math').click(function() {
-    $('.active').removeClass('active');
+//   $('.nametitle').hover(
+//     function(){
+//       $('.about_name_shousai1').fadeIn(4000);
+//   },
+//   function(){
+// $('.about_name_shousai1').fadeOut(1000);
+//   }
+//   );
+//   $('.aboutbtn_math').click(function() {
+//     $('.active').removeClass('active');
     
-    // 変数clickedIndexを定義し、クリックしたボタンのインデックス番号を代入
-    var clickedIndex = $('.aboutbtn_math').index($(this));
+//     // 変数clickedIndexを定義し、クリックしたボタンのインデックス番号を代入
+//     var clickedIndex = $('.aboutbtn_math').index($(this));
     
-    // eqの引数をclickedIndexに書き換え
-    $('.slide').eq(clickedIndex).addClass('active');
+//     // eqの引数をclickedIndexに書き換え
+//     $('.slide').eq(clickedIndex).addClass('active');
     
-  });
-});
-});
+//   });
+// });
+// });
