35fc83b9b39d166c69b9eedd29d79588c3c12457
kdog0625 <kdoooog34@gmail.com>
2020-08-02 01:20:36 +0900

    user.rbテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"kakao"}
+    email                 {"kkk1@gmail.com"}
+    password              {"00000000s"}
+    password_confirmation {"00000000s"}
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "nameがない場合は登録できないこと" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it " 重複したemailが存在する場合は登録できないこと " do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+  end
+end
