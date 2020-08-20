32d8d25ba613e40b99b2b6a4f749c5cf0d9e6020
kdog0625 <kdoooog34@gmail.com>
2020-08-21 08:45:17 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,21 +2,21 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-# if Rails.env.production?
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     region: ENV['AWS_S3_REGION'],
-#     aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-#     aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
-#   }
+if Rails.env.production?
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    region: ENV['AWS_S3_REGION'],
+    aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+  }
 
-#   config.fog_directory  = 'kstargram0625'
-#   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-#   # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kstargram0625'
-# end
+  config.fog_directory  = 'kstargram0625'
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kstargram0625'
+end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
