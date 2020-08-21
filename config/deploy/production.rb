16727156a64ffecbe93c77cff1f68f43ae7b21bb
kdog0625 <kdoooog34@gmail.com>
2020-08-21 23:40:34 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.180.133.172/', user: 'ec2-user', roles: %w{app db web}
+server '18.180.133.172', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
