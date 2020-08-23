d0997e6eabf7b7952f666e4186b7ac1141e52700
kdog0625 <kdoooog34@gmail.com>
2020-08-23 19:46:26 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,18 @@
-app_path = File.expand_path('/var/www/KStargram', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
+
 
 worker_processes 1
 # currentを指定
 working_directory "#{app_path}/current"
 
 # それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
