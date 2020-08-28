4e70c13c5e2f9c9b36f6578cd8605a04b3566b13
kdog0625 <kdoooog34@gmail.com>
2020-08-29 01:47:53 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'KStargram'
 set :deploy_to, "/var/www/KStargram"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/kdog0625/KStargram.git'
+set :repo_url,  'git@github.com:kdog0625/KStargram.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
